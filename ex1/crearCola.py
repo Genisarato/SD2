507ef6164e8e436e5358799ac1752818a74400b8
Genis Aragones <genisarato@gmail.com>
2025-05-21 11:37:56 +0200

    Modificació varia

--- ex1/crearCola.py
+++ ex1/crearCola.py
@@ -1,7 +1,7 @@
 import pika
 
 credentials = pika.PlainCredentials('user', 'password123')
-connection = pika.BlockingConnection(pika.ConnectionParameters('54.167.85.155', 5672, '/', credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
 channel = connection.channel()
 
 channel.queue_declare(queue='InsultQueue', durable=True) 
